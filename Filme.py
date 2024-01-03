343ed6df7a7ea6cb973071424b09452530b91c8c
Jhonata Williams <jhonatawwxx@gmail.com>
2024-01-02 22:23:00 -0300

    Feat: Criação da visualização da lista e remoção por item especifico, popup ao deixar mouse em cima.

--- Filme.py
+++ Filme.py
@@ -5,36 +5,44 @@ import random
 
 class SorteadorFilme:
     def __init__(self, root):
+
+        self.filmes = []
+
         self.root = root
         self.root.title("Sorteador de Filmes")
         self.root.geometry("600x700")
         self.root.configure(bg="#3c3f41")
-        self.filmes = []
-        self.label = tk.Label(root, text="Digite um filme", bg="#3c3f41", font=("Arial", 12, "bold"), fg="white")
+
+        self.label = tk.Label(root, text="Digite um filme", bg="#3c3f41", font=("Helvetica", 24, "bold"), fg="white")
         self.label.pack(ipady=50)
+
         self.entry = tk.Entry(root, width=50)
         self.entry.pack(pady=20)
         self.entry.bind("<Return>", lambda event=None: self.adicionar_string())
+
         self.adicionar_button = tk.Button(root, text="Adicionar filme", command=self.adicionar_string, width=20, height=2, bg="lightgreen")
         self.adicionar_button.pack(pady=20)
+
         self.sortear_button = tk.Button(root, text="Sortear", command=self.sortear_string, width=20, height=2, bg="#FFD700")
         self.sortear_button.pack()
 
-        self.labelLista = tk.Label(root, text="Lista de Filmes", bg="#3c3f41", font=("Arial", 8, "bold"), fg="white")
+        self.labelLista = tk.Label(root, text="Lista de Filmes", bg="#3c3f41", font=("Helvetica", 12, "bold"), fg="white")
         self.labelLista.pack(pady=(60, 0), ipady=0)
 
         self.listbox = tk.Listbox(root, selectmode=tk.SINGLE, width=50, height=10)
         self.listbox.pack(pady=20)
+        self.listbox.bind('<Enter>', self.iniciar_temporizador)
+        self.listbox.bind('<Leave>', self.parar_temporizador)
+        self.temporizador_id = None
 
-        self.limpar = tk.Button(root, text="Limpar", command=self.limpar, width=20, height=2, bg="#FFD700")
+        self.limpar = tk.Button(root, text="Limpar", command=self.limpar, width=20, height=2, bg="#FFFFFF")
         self.limpar.pack()
 
-        # Associa o evento de seleção e a tecla Delete ao Listbox
-        self.listbox.bind('<<ListboxSelect>>', self.obter_selecionado)
-        root.bind('<Delete>', self.remover_item)
+        self.selected_index = None
+        self.listbox.bind('<Double-Button-1>', self.obter_selecionado)
+        root.bind('<Double-Button-1>', self.remover_item)
 
     def obter_selecionado(self, event):
-        # Obtém o índice do item selecionado
         selected_index = self.listbox.curselection()
         if selected_index:
             self.selected_index = selected_index[0]
@@ -42,10 +50,16 @@ class SorteadorFilme:
             self.selected_index = None
 
     def remover_item(self, event):
-        # Certifica-se de que um item foi realmente selecionado
+        index = self.listbox.curselection()
+        if index:
+            self.selected_index = index[0]
+        else:
+            self.selected_index = None
+
         if self.selected_index is not None:
-            # Remove o item selecionado pelo índice
             self.listbox.delete(self.selected_index)
+            self.filmes.pop(self.selected_index)
+            self.selected_index = None
 
     def adicionar_string(self):
         nova_string = self.entry.get()
@@ -64,6 +78,22 @@ class SorteadorFilme:
 
     def limpar(self):
         self.listbox.delete(0, tk.END)
+        self.filmes = []
+
+    def iniciar_temporizador(self, event):
+        self.temporizador_id = self.root.after(1000, self.mostrar_caixa_dialogo)
+
+    def parar_temporizador(self, event):
+        if self.temporizador_id:
+            self.root.after_cancel(self.temporizador_id)
+            self.temporizador_id = None
+
+    def mostrar_caixa_dialogo(self):
+        selected_index = self.listbox.curselection()
+        if selected_index:
+            selected_item = self.listbox.get(selected_index)
+            messagebox.showinfo("Filme Selecionado", f"Click duplo remove o filme: {selected_item}")
+
 
 if __name__ == "__main__":
     root = tk.Tk()
